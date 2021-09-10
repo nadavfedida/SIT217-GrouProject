d8ff6f420761e9acee9ce1d83e0edba7c5896400
Nadav Fedida <62866537+nadavfedida@users.noreply.github.com>
2021-09-10 11:42:54 +1000

    This is the code for the rover

---
+++ Dabble_car_nadav.ino
@@ -0,0 +1,100 @@
+#define CUSTOM_SETTINGS
+#define INCLUDE_GAMEPAD_MODULE
+#include <Dabble.h>
+
+//Right motor
+int enableRightMotor=5; 
+int rightMotorPin1=7;
+int rightMotorPin2=8;
+
+//Left motor
+int enableLeftMotor=6;
+int leftMotorPin1=9;
+int leftMotorPin2=10;
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  pinMode(enableRightMotor,OUTPUT);
+  pinMode(rightMotorPin1,OUTPUT);
+  pinMode(rightMotorPin2,OUTPUT);
+  
+  pinMode(enableLeftMotor,OUTPUT);
+  pinMode(leftMotorPin1,OUTPUT);
+  pinMode(leftMotorPin2,OUTPUT);
+
+  rotateMotor(0,0);   
+    
+  Dabble.begin(9600, 2, 3);
+
+}
+
+
+
+void loop()
+{
+  int rightMotorSpeed=0;
+  int leftMotorSpeed=0;
+  Dabble.processInput();
+  if (GamePad.isUpPressed())
+  {
+    rightMotorSpeed = 150;//255
+    leftMotorSpeed = 150;//255
+    Serial.println("Up");
+
+  }
+
+  if (GamePad.isDownPressed())
+  {
+    Serial.println("down");
+    rightMotorSpeed = -150;//-255
+    leftMotorSpeed = -150;//-255
+    
+  }
+
+  if (GamePad.isLeftPressed())
+  {
+    Serial.println("left");
+    rightMotorSpeed = 100; // 50
+    leftMotorSpeed = 150;//255
+  }
+
+  if (GamePad.isRightPressed())
+  {
+    Serial.println("right");
+    rightMotorSpeed = 150;//255
+    leftMotorSpeed = 100; // 50
+  }
+
+  rotateMotor(rightMotorSpeed, leftMotorSpeed);
+}
+
+void rotateMotor(int rightMotorSpeed, int leftMotorSpeed)
+{
+  if (rightMotorSpeed < 0)
+  {
+    digitalWrite(rightMotorPin1,LOW);
+    digitalWrite(rightMotorPin2,HIGH);    
+  }
+  else if (rightMotorSpeed >= 0)
+  {
+    digitalWrite(rightMotorPin1,HIGH);
+    digitalWrite(rightMotorPin2,LOW);      
+  }
+
+  if (leftMotorSpeed < 0)
+  {
+    digitalWrite(leftMotorPin1,LOW);
+    digitalWrite(leftMotorPin2,HIGH);    
+  }
+  else if (leftMotorSpeed >= 0)
+  {
+    digitalWrite(leftMotorPin1,HIGH);
+    digitalWrite(leftMotorPin2,LOW);      
+  }
+
+  analogWrite(enableRightMotor, abs(rightMotorSpeed));
+  analogWrite(enableLeftMotor, abs(leftMotorSpeed));    
+}
